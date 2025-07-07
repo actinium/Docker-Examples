cc78b5af183af0dba631a068c8bd03e79cf27c93
Andreas Cederholm <andreas.cederholm@gmail.com>
2025-07-07 14:06:19 +0200

    Add PHP example

---
+++ Nginx-PHP/docker/php-fpm/app/index.php
@@ -0,0 +1,34 @@
+<?php
+
+function view($view)
+{
+    require sprintf('%s/views/%s.php', __DIR__, $view);
+}
+
+function redirect($url)
+{
+    http_response_code(302);
+    header('Location: ' . $url);
+}
+
+function show404()
+{
+    http_response_code(404);
+    view('404.html');
+}
+
+function hello()
+{
+    view('hello.html');
+}
+
+function main()
+{
+    match ($_SERVER['DOCUMENT_URI']) {
+        '/' => redirect('/hello'),
+        '/hello' => hello(),
+        default => show404(),
+    };
+}
+
+main();
